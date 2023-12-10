9a2b2ae25cf31566bbf709524473decaa64b64fb
Anshulkhambe <151910504+Anshulkhambe@users.noreply.github.com>
2023-12-10 15:35:16 +0530

    Add files via upload

---
+++ UserAccount.java
@@ -0,0 +1,19 @@
+
+
+public class UserAccount {
+    double balance;
+    int pin;
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+}
